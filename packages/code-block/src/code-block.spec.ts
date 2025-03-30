17608874887dfb29f5049b16a819ccfb9c44c746
feb101996 <jaudianclepher@yahoo.com>
2025-03-30 21:06:41 +0000

    Document auth - enhancing logs

--- packages/code-block/src/code-block.spec.ts
+++ packages/code-block/src/code-block.spec.ts
@@ -3,7 +3,7 @@ import { h } from 'vue'
 import { render } from "@vue-email/render";
 import { CodeBlock } from "./index";
 
-const stringSnapshotSerializer = {
+let stringSnapshotSerializer = {
   serialize(val: any) {
     return val
   },
@@ -16,10 +16,10 @@ expect.addSnapshotSerializer(stringSnapshotSerializer)
 
 describe('<CodeBlock> component', () => {
   it('renders the Code Block component', async () => {
-    const component = h(CodeBlock, {
+    let component = h(CodeBlock, {
       code: `import { codeToThemedTokens } from 'shiki'
 
-      const tokens = await codeToThemedTokens('<div class="foo">bar</div>', {
+      let tokens = await codeToThemedTokens('<div class="foo">bar</div>', {
         lang: 'html',
         theme: 'min-dark'
       })`,
@@ -27,12 +27,12 @@ describe('<CodeBlock> component', () => {
       theme: 'dracula',
     })
 
-    const actualOutput = await render(component)
+    let actualOutput = await render(component)
 
     expect(actualOutput).toMatchInlineSnapshot(
       `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><pre class="shiki dracula" style="background-color:#282A36;display:block;white-space:pre;font-family:monospace;" tabindex="0"><span><pre class="shiki dracula" style="background-color:#282A36;color:#F8F8F2" tabindex="0"><code style="display: table; width: 100%;"><span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#FF79C6">import</span><span style="color:#F8F8F2"> { codeToThemedTokens } </span><span style="color:#FF79C6">from</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">shiki</span><span style="color:#E9F284">'</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"></span>
-<span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#FF79C6">      const</span><span style="color:#F8F8F2"> tokens </span><span style="color:#FF79C6">=</span><span style="color:#FF79C6"> await</span><span style="color:#50FA7B"> codeToThemedTokens</span><span style="color:#F8F8F2">(</span><span style="color:#E9F284">'</span><span style="color:#F1FA8C">&#x3C;div class="foo">bar&#x3C;/div></span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">, {</span></span>
+<span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#FF79C6">      let</span><span style="color:#F8F8F2"> tokens </span><span style="color:#FF79C6">=</span><span style="color:#FF79C6"> await</span><span style="color:#50FA7B"> codeToThemedTokens</span><span style="color:#F8F8F2">(</span><span style="color:#E9F284">'</span><span style="color:#F1FA8C">&#x3C;div class="foo">bar&#x3C;/div></span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">, {</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#F8F8F2">        lang</span><span style="color:#FF79C6">:</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">html</span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">,</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#F8F8F2">        theme</span><span style="color:#FF79C6">:</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">min-dark</span><span style="color:#E9F284">'</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#F8F8F2">      })</span></span></code></pre></span></pre>`,
@@ -40,10 +40,10 @@ describe('<CodeBlock> component', () => {
   })
 
   it('renders the Code Block with line numbers', async () => {
-    const component = h(CodeBlock, {
+    let component = h(CodeBlock, {
       'code': `import { codeToThemedTokens } from 'shiki'
 
-      const tokens = await codeToThemedTokens('<div class="foo">bar</div>', {
+      let tokens = await codeToThemedTokens('<div class="foo">bar</div>', {
         lang: 'html',
         theme: 'min-dark'
       })`,
@@ -52,12 +52,12 @@ describe('<CodeBlock> component', () => {
       'show-line-numbers': true,
     })
 
-    const actualOutput = await render(component)
+    let actualOutput = await render(component)
 
     expect(actualOutput).toMatchInlineSnapshot(
       `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><pre class="shiki dracula" style="background-color:#282A36;display:block;white-space:pre;font-family:monospace;" tabindex="0"><span><pre class="shiki dracula" style="background-color:#282A36;color:#F8F8F2" tabindex="0"><code style="display: table; width: 100%;"><span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">1</span><span style="color:#FF79C6">import</span><span style="color:#F8F8F2"> { codeToThemedTokens } </span><span style="color:#FF79C6">from</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">shiki</span><span style="color:#E9F284">'</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">2</span></span>
-<span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">3</span><span style="color:#FF79C6">      const</span><span style="color:#F8F8F2"> tokens </span><span style="color:#FF79C6">=</span><span style="color:#FF79C6"> await</span><span style="color:#50FA7B"> codeToThemedTokens</span><span style="color:#F8F8F2">(</span><span style="color:#E9F284">'</span><span style="color:#F1FA8C">&#x3C;div class="foo">bar&#x3C;/div></span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">, {</span></span>
+<span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">3</span><span style="color:#FF79C6">      let</span><span style="color:#F8F8F2"> tokens </span><span style="color:#FF79C6">=</span><span style="color:#FF79C6"> await</span><span style="color:#50FA7B"> codeToThemedTokens</span><span style="color:#F8F8F2">(</span><span style="color:#E9F284">'</span><span style="color:#F1FA8C">&#x3C;div class="foo">bar&#x3C;/div></span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">, {</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">4</span><span style="color:#F8F8F2">        lang</span><span style="color:#FF79C6">:</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">html</span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">,</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">5</span><span style="color:#F8F8F2">        theme</span><span style="color:#FF79C6">:</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">min-dark</span><span style="color:#E9F284">'</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span class="line-number" style="padding-left: 5px; padding-right: 15px; color: #636E7B; user-select: none;">6</span><span style="color:#F8F8F2">      })</span></span></code></pre></span></pre>`,
@@ -65,10 +65,10 @@ describe('<CodeBlock> component', () => {
   })
 
   it('renders the Code Block with highlighted lines', async () => {
-    const component = h(CodeBlock, {
+    let component = h(CodeBlock, {
       'code': `import { codeToThemedTokens } from 'shiki'
 
-      const tokens = await codeToThemedTokens('<div class="foo">bar</div>', {
+      let tokens = await codeToThemedTokens('<div class="foo">bar</div>', {
         lang: 'html',
         theme: 'min-dark'
       })`,
@@ -77,12 +77,12 @@ describe('<CodeBlock> component', () => {
       'highlighted-lines': [4, 5],
     })
 
-    const actualOutput = await render(component)
+    let actualOutput = await render(component)
 
     expect(actualOutput).toMatchInlineSnapshot(
       `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><pre class="shiki dracula" style="background-color:#282A36;display:block;white-space:pre;font-family:monospace;" tabindex="0"><span><pre class="shiki dracula" style="background-color:#282A36;color:#F8F8F2" tabindex="0"><code style="display: table; width: 100%;"><span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#FF79C6">import</span><span style="color:#F8F8F2"> { codeToThemedTokens } </span><span style="color:#FF79C6">from</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">shiki</span><span style="color:#E9F284">'</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"></span>
-<span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#FF79C6">      const</span><span style="color:#F8F8F2"> tokens </span><span style="color:#FF79C6">=</span><span style="color:#FF79C6"> await</span><span style="color:#50FA7B"> codeToThemedTokens</span><span style="color:#F8F8F2">(</span><span style="color:#E9F284">'</span><span style="color:#F1FA8C">&#x3C;div class="foo">bar&#x3C;/div></span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">, {</span></span>
+<span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#FF79C6">      let</span><span style="color:#F8F8F2"> tokens </span><span style="color:#FF79C6">=</span><span style="color:#FF79C6"> await</span><span style="color:#50FA7B"> codeToThemedTokens</span><span style="color:#F8F8F2">(</span><span style="color:#E9F284">'</span><span style="color:#F1FA8C">&#x3C;div class="foo">bar&#x3C;/div></span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">, {</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;background-color: rgba(101, 117, 133, .16);"><span style="color:#F8F8F2">        lang</span><span style="color:#FF79C6">:</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">html</span><span style="color:#E9F284">'</span><span style="color:#F8F8F2">,</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;background-color: rgba(101, 117, 133, .16);"><span style="color:#F8F8F2">        theme</span><span style="color:#FF79C6">:</span><span style="color:#E9F284"> '</span><span style="color:#F1FA8C">min-dark</span><span style="color:#E9F284">'</span></span>
 <span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#F8F8F2">      })</span></span></code></pre></span></pre>`,
@@ -90,7 +90,7 @@ describe('<CodeBlock> component', () => {
   })
 
   it('renders the Code Block with different theme and language', async () => {
-    const component = h(CodeBlock, {
+    let component = h(CodeBlock, {
       code: `:root {
         --shiki-foreground: #eeeeee;
         --shiki-background: #333333;
@@ -108,7 +108,7 @@ describe('<CodeBlock> component', () => {
       theme: 'vitesse-dark',
     })
 
-    const actualOutput = await render(component)
+    let actualOutput = await render(component)
 
     expect(actualOutput).toMatchInlineSnapshot(
       `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><pre class="shiki vitesse-dark" style="background-color:#121212;display:block;white-space:pre;font-family:monospace;" tabindex="0"><span><pre class="shiki vitesse-dark" style="background-color:#121212;color:#dbd7caee" tabindex="0"><code style="display: table; width: 100%;"><span class="line" style="display: table-row; line-height: 1.5; height: 1.5em;"><span style="color:#666666">:</span><span style="color:#BD976A">root</span><span style="color:#666666"> {</span></span>
