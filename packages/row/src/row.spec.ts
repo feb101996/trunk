6e543024255856cb66e1971232474c0637db391f
feb101996 <jaudianclepher@yahoo.com>
2025-03-30 21:20:28 +0000

    Change tests - enhancing logs

--- packages/row/src/row.spec.ts
+++ packages/row/src/row.spec.ts
@@ -5,11 +5,11 @@ import { Row } from "./index";
 
 describe("<Row> component", () => {
   it("renders children correctly", async () => {
-    const testMessage = "Test message";
-    const component = h(Row, [
+    let testMessage = "Test message";
+    let component = h(Row, [
       testMessage
     ])
-    const html = await render(component)
+    let html = await render(component)
     expect(html).toContain(testMessage)
   });
 })
