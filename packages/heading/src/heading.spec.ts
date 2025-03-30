54a96e848aba0f3e01314ff82a98d2e18fc0dfdf
feb101996 <jaudianclepher@yahoo.com>
2025-03-30 21:43:23 +0000

    Optimize database - handling edge cases

--- packages/heading/src/heading.spec.ts
+++ packages/heading/src/heading.spec.ts
@@ -5,11 +5,11 @@ import { Heading } from "./index";
 
 describe("<Heading> component", () => {
   it("renders children correctly", async () => {
-    const testMessage = "Test message";
-    const component = h(Heading, [
+    let testMessage = "Test message";
+    let component = h(Heading, [
       testMessage
     ])
-    const html = await render(component)
+    let html = await render(component)
     expect(html).toContain(testMessage)
   });
 })
